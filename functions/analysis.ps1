065ea8a46da53f1126af9462997c141e5e400dea
Nemo Glade <ige@ascendispharma.com>
2025-04-24 10:45:49 +0200

    analysis rennt. i am unconvinced of the output but at least we are getting somewhere with sites. also module install now has an error output :3

--- functions/analysis.ps1
+++ functions/analysis.ps1
@@ -6,13 +6,42 @@
 #     - all GPOs in active use (linked to OU and link Enabled)
 
 
+# site level GPOs cause GPInheritance is a bitch and doesn't like sites
+function GetSiteLevel
+{
+    $siteDNprefix = "CN=Sites,"+([ADSI]"LDAP://RootDSE").configurationNamingContext
+    $sites = Get-ADObject -SearchBase $siteDNprefix -Filter 'objectClass -eq "site"' -Properties gPLink
+
+    $siteGPOlinks = @()
+    foreach ($site in $sites) {
+        if ($site.gPLink) {
+            $site.gPLink -split '\[LDAP://' | ForEach-Object {
+                if ($_ -match '^CN={(.*?)}.*;([01])\]') {
+                    $gpoGuid = $matches[1]
+                    $enabled = [int]$matches[2]
+                    $gpo = Get-GPO -Guid $gpoGuid -ErrorAction SilentlyContinue
+                    if ($gpo) {
+                        $siteGPOlinks += [PSCustomObject]@{
+                            DisplayName = $gpo.DisplayName
+                            Guid        = $gpo.Id
+                            Enabled     = $enabled
+                            Site        = $site.Name }
+                    }
+                }
+            }
+        }
+    }
+    return $siteGPOlinks
+}
+
 function Analysis  
 {
     # check and import GPO module
 
     if (-Not (Get-Module -ListAvailable -Name GroupPolicy))
     {
-        return false
+        Write-Error "Please make sure you have installed the GroupPolicy Module! Otherwise this will be a very unhelpful time."
+        return
     }
     Import-Module GroupPolicy
 
@@ -27,6 +56,7 @@ function Analysis
 
     # OU level
     $ouGPOlinks = @()
+    $oulinkedGPOs = @()
     Get-ADOrganizationalUnit -Filter * | ForEach-Object {
         $ouGPOlinks += (Get-GPInheritance -Target $_.DistinguishedName).GpoLinks }
     $oulinkedGPOs = $ouGPOlinks | Where-Object { $_.DisplayName -ne $null }
@@ -34,17 +64,19 @@ function Analysis
     $oulinkedGPOsACTIVE = $oulinkedGPOs | Where-Object {$_.Enabled -ne "None"}
 
     # site level
-    #$sitelinkedGPOs = @()
+    #$siteGPOlinks = @()
+    $sitelinkedGPOs = @()
     #Get-ADReplicationSite -Filter * | ForEach-Object {
-    #    $sitelinkedGPOs += Get-GPLink -Target ("CN=" + $_.Name + ",CN=Sites,CN=Configuration," + ([ADSI]"LDAP://RootDSE").configurationNamingContext) }
-    #$sitelinkedGPOsACTIVE = $sitelinkedGPOs | Where-Object {$_.Enabled -eq $true}
+    #    $siteGPOlinks += Get-GPLink -Target ("CN=" + $_.Name + ",CN=Sites,CN=Configuration," + ([ADSI]"LDAP://RootDSE").configurationNamingContext) }
+    $siteGPOlinks = GetSiteLevel
+    #$sitelinkedGPOsACTIVE = $sitelinkedGPOs | Where-Object {$_.Enabled == 1}
 
     # total
     $allGPOsCount = (Get-ADObject -Filter { objectClass -eq "groupPolicyContainer" } -SearchBase "CN=Policies,CN=System,$((Get-ADDomain).DistinguishedName)").Count
     $alllinkedGPOs = @()
     $alllinkedGPOs += $domainlinkedGPOs
     $alllinkedGPOs += $oulinkedGPOs
-    #$alllinkedGPOs += $sitelinkedGPOs
+    $alllinkedGPOs += $sitelinkedGPOs
     $alllinkedGPOs = $alllinkedGPOs | Where-Object { $_.DisplayName -ne $null } 
     $alllinkedGPOs = $alllinkedGPOs | Sort-Object -Property Guid, @{Expression = {$_."Enabled" -eq "None"}; Descending=$true} -Unique
     $alllinkedGPOsACTIVE = $alllinkedGPOs | Where-Object {$_.Enabled -ne "None"}
@@ -67,10 +99,11 @@ function Analysis
     Write-Output "OU-level linked GPOs: $($oulinkedGPOs.Count)"
     Write-Output "OU-level linked GPOs with Enabled links: $($oulinkedGPOsACTIVE.Count)"
 
-    #Write-Output ""
-    #Write-Output "Site-level linked GPOs: $($sitelinkedGPOs.Count)"
-    #Write-Output "Site-level linked GPOs with Enabled links: $($sitelinkedGPOsACTIVE.Count)"
+    Write-Output ""
+    Write-Output "Site-level links to GPOs: $($siteGPOlinks.Count)"
+    Write-Output "Site-level linked GPOs: $($sitelinkedGPOs.Count)"
+    Write-Output "Site-level linked GPOs with Enabled links: $($sitelinkedGPOsACTIVE.Count)"
 
     }
 
-    Analysis
+Analysis
